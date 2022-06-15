507afe200a9c74eb7cc25d1c5ac0f643b0ad6778
Soyab Mostofa <soyab@pop-os.localdomain>
2022-06-15 19:05:20 +0600

    added flair

--- src/components/HomeComp/Hero.jsx
+++ src/components/HomeComp/Hero.jsx
@@ -5,11 +5,11 @@ import tw from 'tailwind-styled-components';
 import Parallax from 'components/layout/Parallax';
 const Container = tw(
   motion.div
-)`container px-4 overflow-hidden mx-auto md:py-72 py-40  max-w-6xl h-screen relative`;
+)`container px-4 overflow-hidden mx-auto md:py-72 py-40  max-w-6xl h-[700px] relative z-20 h-[800px]`;
 const Heading = tw(motion.h1)`text-4xl md:text-6xl font-semibold`;
 const Sub = tw.p`text-xl md:text-2xl font-semibold my-4`;
 const P = tw(motion.p)`md:text-lg text-sm mb-4 `;
-const Background = tw.div`bg-gradient-to-tr from-neutral-900 to-amber-900 text-neutral-50 `;
+const Background = tw.div`bg-gradient-to-tr from-neutral-900 to-amber-900 text-neutral-50 relative `;
 const Button = tw.button`text-neutral-50 px-6 py-3 tracking-wide transition-all hover:text-neutral-900 hover:bg-neutral-300 uppercase bg-black`;
 
 const containerVariants = {
@@ -36,25 +36,21 @@ const Hero = () => {
           animate="animate"
         >
           <Parallax>
-            <div className="relative z-20">
-              <Heading>3D VIRTUAL TOURS</Heading>
-              <Sub>HIGH-FIDELITY INTERACTIVE INTERIOR WALK-THROUGHS</Sub>
-              <P>
-                Auleek’s 3D interactive virtual solution is the ideal way for
-                you to showcase your space. Whether for marketing of your
-                spaces, showcasing your interior design, conservation of design
-                heritage, or anything else, our technology offers the best
-                visual quality, smoothest navigation and most extensive feature
-                set.
-              </P>
-              <Button>get a quote</Button>
-            </div>
+            <Heading>3D VIRTUAL TOURS</Heading>
+            <Sub>HIGH-FIDELITY INTERACTIVE INTERIOR WALK-THROUGHS</Sub>
+            <P>
+              Auleek’s 3D interactive virtual solution is the ideal way for you
+              to showcase your space. Whether for marketing of your spaces,
+              showcasing your interior design, conservation of design heritage,
+              or anything else, our technology offers the best visual quality,
+              smoothest navigation and most extensive feature set.
+            </P>
+            <Button>get a quote</Button>
           </Parallax>
-          <div className="absolute bottom-0 right-0">
-            <Spline scene="https://prod.spline.design/kNCj3cBhjiCk3nNY/scene.splinecode" />
-            <div className="absolute top-0 w-full h-full bg-transparent" />
-          </div>
         </Container>
+        <div className="absolute bottom-0 right-0 z-0 w-full h-full">
+          <Spline scene="https://prod.spline.design/sFKlXcOdIglFLs3d/scene.splinecode" />
+        </div>
       </Background>
     </>
   );
